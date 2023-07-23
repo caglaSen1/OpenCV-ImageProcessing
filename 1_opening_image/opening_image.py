54a7f3a6f771486f2d138b5053f01abd408aa11b
Çağla Şen <caglasenn1@gmail.com>
2023-07-23 10:44:01 +0300

    Add files via upload

---
+++ 1_opening_image/opening_image.py
@@ -0,0 +1,24 @@
+import cv2
+
+# Normal Image Name
+img_name = "messi.jpg"
+
+# Import Normal Image
+img_normal = cv2.imread(img_name)
+
+# Show Normal Image
+cv2.imshow("Normal Image", img_normal)
+
+# Import As Gray Scale
+img_gray = cv2.imread(img_name, 0)  # 0 allows to import as gray scale
+
+# Show Gray Scale Image
+cv2.imshow("Gray Image", img_gray)
+
+# Connecting keyboard
+key = cv2.waitKey(0) & 0xFF
+if key == 27:  # (27 == esc)
+    cv2.destroyAllWindows()
+elif key == ord('s'):
+    cv2.imwrite("messi_gray.png", img_gray)  # Saving normal image in gray scale
+    cv2.destroyAllWindows()
